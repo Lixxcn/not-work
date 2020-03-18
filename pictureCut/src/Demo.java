b5ea755dc01364aa07241274458939d7631c8480
LxxDawn <li.xiaoxu@hotmail.com>
2020-03-18 09:29:16 +0800

    海贼王漫画裁剪增加选择单独文件裁切功能。

--- pictureCut/src/Demo.java
+++ pictureCut/src/Demo.java
@@ -17,67 +17,99 @@ public class Demo {
 //        BufferedImage bii=img_tailor(bi,0,0,1560,2400);
 //        img2file(bii,"jpg","F:\\dawn\\onepiece\\color\\ONE.PIECE.COLOR.HS.01-50[Chinese]海賊王彩漫中文版[高分辨率]\\处理后\\java\\test3.jpg");  //生成图片
 
-        File indir = new File(str);
-        File outdir = new File("F:\\java-test\\out2");
-        String[] picturesName = indir.list();
-        for (int i = 0; i < picturesName.length; i++) {
-            try {
-                Rgb rgbStan = new Rgb();
-                rgbStan.setRgbR(255);
-                rgbStan.setRgbG(255);
-                rgbStan.setRgbB(255);
-                ArrayList<Rgb>  rgbList = getImageGRB(indir.getPath() + File.separator + picturesName[i]);
+        if(true){
+            solo();
+        }else {
+
+            File indir = new File(str);
+            File outdir = new File("F:\\java-test\\out2");
+            String[] picturesName = indir.list();
+            for (int i = 0; i < picturesName.length; i++) {
+                try {
+                    Rgb rgbStan = new Rgb();
+                    rgbStan.setRgbR(255);
+                    rgbStan.setRgbG(255);
+                    rgbStan.setRgbB(255);
+                    Rgb rgbStanBlack = new Rgb();
+                    rgbStanBlack.setRgbR(0);
+                    rgbStanBlack.setRgbG(0);
+                    rgbStanBlack.setRgbB(0);
+                    ArrayList<Rgb> rgbList = getImageGRB(indir.getPath() + File.separator + picturesName[i]);
 
 
-                String pictureName = picturesName[i].replace(".jpg","");
-                String chapter = pictureName.split("_")[0];
-                String page = pictureName.split("_")[1];
-                boolean flag = false;
-                for (int i1 = 0; i1 < rgbList.size(); i1 = i1 + 100) {
-                    if(!rgbList.get(i1).equals(rgbStan)){
-                        BufferedImage bi=file2img(indir.getPath() + File.separator + picturesName[i]);
-                        BufferedImage bii=img_tailor(bi,0,0,3120,2400);
-                        img2file(bii,"jpg",outdir.getAbsolutePath() +  File.separator + pictureName + ".jpg");
-                        flag = true;
+                    String pictureName = picturesName[i].replace(".jpg", "");
+                    String chapter = pictureName.split("_")[0];
+                    String page = pictureName.split("_")[1];
+                    boolean flag = false;
+                    for (int i1 = 0; i1 < rgbList.size(); i1 = i1 + 100) {
+                        if (!rgbList.get(i1).equals(rgbStan)) {
+                            BufferedImage bi = file2img(indir.getPath() + File.separator + picturesName[i]);
+                            BufferedImage bii = img_tailor(bi, 0, 0, 3120, 2400);
+                            img2file(bii, "jpg", outdir.getAbsolutePath() + File.separator + pictureName + ".jpg");
+                            flag = true;
+                            continue;
+                        }
+                    }
+                    if (flag) {
                         continue;
                     }
-                }
-                if(flag){
-                    continue;
-                }
-                //左侧的图片为后值，右侧的图片为前值
-                String left = "";
-                String right = "";
-                try {
-                    left = page.split("-")[1] + ".jpg";
-                    right = page.split("-")[0]+ ".jpg";
-                } catch (Exception e) {
-                    if("001".equals(page)){
-                    }else if("002".equals(page)){
-                        BufferedImage bi=file2img(indir.getPath() + File.separator + picturesName[i]);
-                        BufferedImage bii=img_tailor(bi,0,0,1560,2400);
-                        img2file(bii,"jpg",outdir.getAbsolutePath() +  File.separator + page);
-                    }else{
-                        System.out.println(picturesName[i] + "不是内容文件");
+
+                    //左侧的图片为后值，右侧的图片为前值
+                    String left = "";
+                    String right = "";
+                    try {
+                        left = page.split("-")[1] + ".jpg";
+                        right = page.split("-")[0] + ".jpg";
+                    } catch (Exception e) {
+                        if ("001".equals(page)) {
+                        } else if ("002".equals(page)) {
+                            BufferedImage bi = file2img(indir.getPath() + File.separator + picturesName[i]);
+                            BufferedImage bii = img_tailor(bi, 0, 0, 1560, 2400);
+                            img2file(bii, "jpg", outdir.getAbsolutePath() + File.separator + page);
+                        } else {
+                            System.out.println(picturesName[i] + "不是内容文件");
+                        }
                     }
+
+
+                    //处理左侧图片
+                    BufferedImage bi = file2img(indir.getPath() + File.separator + picturesName[i]);
+                    BufferedImage bii = img_tailor(bi, 0, 0, 1560, 2400);
+                    img2file(bii, "jpg", outdir.getAbsolutePath() + File.separator + chapter + "_" + left);
+
+                    //处理右侧图片
+                    BufferedImage biii = img_tailor(bi, 1560, 0, 1560, 2400);
+                    img2file(biii, "jpg", outdir.getAbsolutePath() + File.separator + chapter + "_" + right);
+
+                } catch (Exception e) {
+                    System.out.println(picturesName[i] + "不符合要求，不进行处理");
                 }
-
-
-                //处理左侧图片
-                BufferedImage bi=file2img(indir.getPath() + File.separator + picturesName[i]);
-                BufferedImage bii=img_tailor(bi,0,0,1560,2400);
-                img2file(bii,"jpg",outdir.getAbsolutePath() +  File.separator + chapter + "_" + left);
-
-                //处理右侧图片
-                BufferedImage biii = img_tailor(bi,1560,0,1560,2400);
-                img2file(biii,"jpg",outdir.getAbsolutePath() +  File.separator + chapter + "_" + right);
-
-            } catch (Exception e) {
-                System.out.println(picturesName[i] + "不符合要求，不进行处理");
             }
-        }
 
 //        ArrayList<Rgb> rgbList = getImageGRB(str);
+        }
+
+    }
+    static void solo(){
+        File dir = new File("F:\\java-test\\out");
+        String infilename = "01_135-136.jpg";
+        String pictureName = infilename.replace(".jpg","");
+        String chapter = pictureName.split("_")[0];
+        String page = pictureName.split("_")[1];
+        //左侧的图片为后值，右侧的图片为前值
+        String left = "";
+        String right = "";
+        left = page.split("-")[1] + ".jpg";
+        right = page.split("-")[0]+ ".jpg";
+
+        //处理左侧图片
+        BufferedImage bi=file2img(dir.getPath() + File.separator + infilename);
+        BufferedImage bii=img_tailor(bi,0,0,1560,2400);
+        img2file(bii,"jpg",dir.getAbsolutePath() +  File.separator + chapter + "_" + left);
+
+        //处理右侧图片
+        BufferedImage biii = img_tailor(bi,1560,0,1560,2400);
+        img2file(biii,"jpg",dir.getAbsolutePath() +  File.separator + chapter + "_" + right);
 
     }
     public static BufferedImage img_tailor(BufferedImage src,int x,int y,int width,int height) {
